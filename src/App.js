77cdc7272ea38c14b837a386dd5a6e5c1eb9ab25
Christopher (Cody) Querubin <cody.querubin130@gmail.com>
2024-02-08 11:13:41 -0500

    Start of Portfolio Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Menu from './menu.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div class = "container">
+            <div class = "row">
+                <div class = "col-3"></div>
+                <div class = "col-5">
+                    <h1>Christopher (Cody) Querubin</h1>
+                    <h4>Computer Science At George Mason University</h4>
+                    <div id="root"></div>
+                    <Menu />
+                </div>
+                <div class = "col-3"></div>
+            </div>
+        </div>
     </div>
+    
   );
 }
 
